0ffa9f9b64597349a98cc388ea1ea20687286958
easinrobin <robinkhondokar@gmail.com>
2020-01-15 16:52:31 +0600

    Layout Fix

--- TwAdmin/App_Start/BundleConfig.cs
+++ TwAdmin/App_Start/BundleConfig.cs
@@ -9,22 +9,109 @@ namespace TwAdmin
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-2.1.4.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvalUnobtrusive").Include(
+                "~/Scripts/jquery.validate.unobtrusive.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-2.8.3.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/buttons-html5").Include(
+                "~/Scripts/buttons.html5.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/buttons-print").Include(
+                "~/Scripts/buttons.print.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataTables-buttons").Include(
+                "~/Scripts/dataTables.buttons.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dropzone").Include(
+                "~/Scripts/dropzone.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/flatpickr").Include(
+                "~/Scripts/flatpickr.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-scrollLock").Include(
+                "~/Scripts/jquery-scrollLock.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataTables").Include(
+                "~/Scripts/jquery.dataTables.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scrollbar").Include(
+                "~/Scripts/jquery.scrollbar.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/lightgallery").Include(
+                "~/Scripts/lightgallery-all.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                "~/Scripts/moment.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                "~/Scripts/popper.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/select2").Include(
+                "~/Scripts/select2.full.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/sweetalert2").Include(
+                "~/Scripts/sweetalert2.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/trumbowyg").Include(
+                "~/Scripts/trumbowyg.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/appMin").Include(
+                "~/Scripts/CustomJs/app.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/demo").Include(
+                "~/Scripts/CustomJs/demo.js"));
+
+
+
+
+            // CSS
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/VendorCss/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzone").Include(
+                      "~/Content/VendorCss/dropzone.css"));
+
+            bundles.Add(new StyleBundle("~/Content/animate").Include(
+                "~/Content/VendorCss/animate.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/flatpickr").Include(
+                "~/Content/VendorCss/flatpickr.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/scrollbar").Include(
+                "~/Content/VendorCss/jquery.scrollbar.css"));
+
+            bundles.Add(new StyleBundle("~/Content/lightgallery").Include(
+                "~/Content/VendorCss/lightgallery.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/iconic-font").Include(
+                "~/Content/VendorCss/material-design-iconic-font.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/select2").Include(
+                "~/Content/VendorCss/select2.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/sweetalert2").Include(
+                "~/Content/VendorCss/sweetalert2.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/trumbowyg").Include(
+                "~/Content/VendorCss/trumbowyg.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/appMin").Include(
+                "~/Content/CustomCss/app.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/demo").Include(
+                "~/Content/CustomCss/demo.css"));
         }
     }
 }
--- TwWeb/App_Start/BundleConfig.cs
+++ TwWeb/App_Start/BundleConfig.cs
@@ -8,23 +8,116 @@ namespace TwWeb
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // Scripts
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/jquery").Include(
+                        "~/Scripts/ThirdPartyJs/jquery-1.9.1.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/jqueryval").Include(
+                        "~/Scripts/ThirdPartyJs/jquery.validate.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/jqueryvalUnobtrusive").Include(
+                "~/Scripts/ThirdPartyJs/jquery.validate.unobtrusive.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/modernizr").Include(
+                        "~/Scripts/ThirdPartyJs/modernizr-2.8.3.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/bootstrap").Include(
+                      "~/Scripts/ThirdPartyJs/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/animate").Include(
+                "~/Scripts/ThirdPartyJs/animate.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/inview").Include(
+                "~/Scripts/ThirdPartyJs/inview.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/bxslider").Include(
+                "~/Scripts/ThirdPartyJs/jquery.bxslider.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/cslider").Include(
+                "~/Scripts/ThirdPartyJs/jquery.cslider.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/easing").Include(
+                "~/Scripts/ThirdPartyJs/jquery.easing.1.3.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/flexslider").Include(
+                "~/Scripts/ThirdPartyJs/jquery.flexslider.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/prettyPhoto").Include(
+                "~/Scripts/ThirdPartyJs/jquery.prettyPhoto.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/slick").Include(
+                "~/Scripts/ThirdPartyJs/slick.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/toucheffects").Include(
+                "~/Scripts/ThirdPartyJs/toucheffects.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/camera").Include(
+                "~/Scripts/ThirdPartyJs/camera/camera.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/camera-setting").Include(
+                "~/Scripts/ThirdPartyJs/camera/setting.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/google-code-prettify").Include(
+                "~/Scripts/ThirdPartyJs/google-code-prettify/prettify.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/portfolio").Include(
+                "~/Scripts/ThirdPartyJs/portfolio/jquery.quicksand.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ThirdPartyJs/portfolio-setting").Include(
+                "~/Scripts/ThirdPartyJs/portfolio/setting.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/CustomJs/customJs").Include(
+                "~/Scripts/ThirdPartyJs/camera/custom.js"));
+
+
+
+            // CSS
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/css").Include(
+                      "~/Content/ThirdPartyCss/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/bootstrap-responsive").Include(
+                      "~/Content/ThirdPartyCss/bootstrap-responsive.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/animate").Include(
+                "~/Content/ThirdPartyCss/animate.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/font-awesome").Include(
+                "~/Content/ThirdPartyCss/font-awesome.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/defaultColor").Include(
+                "~/Content/ThirdPartyCss/default.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/cslider").Include(
+                "~/Content/ThirdPartyCss/cslider.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/camera").Include(
+                "~/Content/ThirdPartyCss/camera.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/flexslider").Include(
+                "~/Content/ThirdPartyCss/flexslider.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/bxslider").Include(
+                "~/Content/ThirdPartyCss/jquery.bxslider.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/overwrite").Include(
+                "~/Content/ThirdPartyCss/overwrite.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/prettyPhoto").Include(
+                "~/Content/ThirdPartyCss/prettyPhoto.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/shortcodes").Include(
+                "~/Content/ThirdPartyCss/shortcodes.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/slick").Include(
+                "~/Content/ThirdPartyCss/slick.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ThirdPartyCss/slick-theme").Include(
+                "~/Content/ThirdPartyCss/slick-theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/CustomCss/style").Include(
+                "~/Content/CustomCss/style.css"));
         }
     }
 }
